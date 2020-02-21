4a627a4ab2f3ef4ac6b63d20bfea6bcb595ffba9
faizalyaacob <jalpejal74123@gmail.com>
2020-02-21 19:00:34 +0800

    blur

--- app.py
+++ app.py
@@ -1,22 +1,79 @@
-from flask import Flask, render_template, Response
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, Response
+import os
 from camera import VideoCamera
+from camera import VideoCamera1
+from camera import VideoCamera2
+from camera import VideoCamera3
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def multi_gen(camera_id):
 
+    cam = find_camera(int(camera_id))
+    cap=  cv2.VideoCapture(cam)                                                                 
+    
+    while True:
+        # for cap in caps:
+        # # Capture frame-by-frame
+        success, frame = cap.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result  
+
+
+@app.route('/dashboard')
+def home():
+	 return render_template('dashboard.html')
+	# if not session.get('logged_in'):
+	# 	return render_template('login.html')
+	# else:
+	#	return "You're logged in"
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+#Cam 1             
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+#Cam 2
+@app.route('/video_feed1')
+def video_feed1():
+     return Response(gen(VideoCamera1()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#Cam 3
+@app.route('/video_feed2')
+def video_feed2():
+     return Response(gen(VideoCamera2()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#Cam 4
+@app.route('/video_feed3')
+def video_feed3():
+     return Response(gen(VideoCamera3()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+
+if __name__ == "__main__":
+	app.secret_key = os.urandom(12)
+	app.run(debug=True,host='0.0.0.0')
--- camera.py
+++ camera.py
@@ -10,13 +10,99 @@ from mtcnn import MTCNN
 # face detector
 detector = MTCNN()
 
-class VideoCamera:
+class VideoCamera():
     '''
     Setup source for threading and streaming to flask
     '''
     def __init__(self, src=0):
         # default source from webcam (0), set source as needed
         self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        _, image = self.video.read()
+        # detect faces
+        faces = detector.detect_faces(image)
+        # for faces detected, draw a box around it
+        for face in faces:
+            # get coordinates
+            x1, y1, w, h = face['box']
+            # plot in frame
+            cv2.rectangle(image, (x1, y1), (x1+w, y1+h), (0, 0, 255), 2)
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        #key = self.waitKey(20)
+        #if key == 'p':
+        #   image = not image
+
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+class VideoCamera1:
+    '''
+    Setup source for threading and streaming to flask
+    '''
+    def __init__(self, src=0):
+        # default source from webcam (0), set source as needed
+        self.video = cv2.VideoCapture(1)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        _, image = self.video.read()
+        # detect faces
+        faces = detector.detect_faces(image)
+        # for faces detected, draw a box around it
+        for face in faces:
+            # get coordinates
+            x1, y1, w, h = face['box']
+            # plot in frame
+            cv2.rectangle(image, (x1, y1), (x1+w, y1+h), (0, 0, 255), 2)
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+class VideoCamera2:
+    '''
+    Setup source for threading and streaming to flask
+    '''
+    def __init__(self, src=0):
+        # default source from webcam (0), set source as needed
+        self.video = cv2.VideoCapture(2)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        _, image = self.video.read()
+        # detect faces
+        faces = detector.detect_faces(image)
+        # for faces detected, draw a box around it
+        for face in faces:
+            # get coordinates
+            x1, y1, w, h = face['box']
+            # plot in frame
+            cv2.rectangle(image, (x1, y1), (x1+w, y1+h), (0, 0, 255), 2)
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+class VideoCamera3:
+    '''
+    Setup source for threading and streaming to flask
+    '''
+    def __init__(self, src=0):
+        # default source from webcam (0), set source as needed
+        self.video = cv2.VideoCapture(3)
     
     def __del__(self):
         self.video.release()
