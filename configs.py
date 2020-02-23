25cfb88067726ee5a417d04ff138e8d99a0c7848
taufikalhkm <taufikalhkm@gmail.com>
2020-02-24 00:50:39 +0700

    fix wrong merge

--- configs.py
+++ configs.py
@@ -2,13 +2,12 @@ import os
 #RUNNER = "daus"
 # RUNNER = "chikeong"
 RUNNER = "taufiq"
+HOST_IP = "http://localhost:5000"
 
-
-STORAGE_PATH = "storage"
+STORAGE_PATH = "static/storage"
 
 def assure_path_exists(path):
 	try:
 		dir = os.path.dirname(path)
 	except Exception as e:
 		os.makedirs(dir)
-        
