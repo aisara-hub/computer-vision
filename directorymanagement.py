40f1316394e9b9000659d30acf1fda2a5a183cff
neonexxa <firdaus@cyberhax.club>
2020-02-25 11:48:17 +0800

    fix filter

--- directorymanagement.py
+++ directorymanagement.py
@@ -12,16 +12,16 @@ def watch(folder_path=configs.STORAGE_PATH,fromtime = datetime.now().timestamp()
 
 		# the new one
 		fileList = [file.split('/')[-1] for file in os.listdir (folder_path) if '.' in file]
-		print(fileList,fromtime,totime)
+		# print(fileList,fromtime,totime)
 		fileList_filtered_bydatetime = [filename for filename in fileList if (int(int(fromtime)/1000)<=int("".join(filename.split('.')[:-2]).split('_')[1])<=int(int(totime)/1000))]
 		# 1582598025
-		for filename in fileList:
-			print("what is this 1: ",filename.split('.')[:-2])
-			print("what the hell is this: ",int("".join(filename.split('.')[:-2]).split('_')[1]))
-			print(int(int(fromtime)/1000))
-			print(int(int(totime)/1000))
-			print(int(int(fromtime)/1000)<=int("".join(filename.split('.')[:-2]).split('_')[1])<=int(int(totime)/1000))
-			if ((int(fromtime)/1000)>=int("".join(filename.split('.')[:-2]).split('_')[1])>=(int(totime)/1000)):
-				pass
-		print("after filter",fileList_filtered_bydatetime,fromtime,totime)
+		# for filename in fileList:
+		# 	print("what is this 1: ",filename.split('.')[:-2])
+		# 	print("what the hell is this: ",int("".join(filename.split('.')[:-2]).split('_')[1]))
+		# 	print(int(int(fromtime)/1000))
+		# 	print(int(int(totime)/1000))
+		# 	print(int(int(fromtime)/1000)<=int("".join(filename.split('.')[:-2]).split('_')[1])<=int(int(totime)/1000))
+		# 	if ((int(fromtime)/1000)>=int("".join(filename.split('.')[:-2]).split('_')[1])>=(int(totime)/1000)):
+		# 		pass
+		# print("after filter",fileList_filtered_bydatetime,fromtime,totime)
 		return fileList_filtered_bydatetime
