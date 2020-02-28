702dc49d76ede6e1ea48e64f8dcf026c6c10ae5f
Chikeong <chikeonglo@outlook.com>
2020-02-28 18:02:54 +0800

    future plans

---
+++ facial_recogniser.py
@@ -0,0 +1,16 @@
+# libraries
+
+# steps
+# 1. Build system for list of know faces
+#       a. process face
+#       b. embedding of face
+#       c. save locally for retrieval
+# 2. Build system for faces detected by system
+#       a. process face
+#       b. embeddings
+# 3. Predictive model
+#       a. load known embeddings
+#       b. normalisation
+#       c. encoding
+#       d. choose a model
+#       e. train and predict
